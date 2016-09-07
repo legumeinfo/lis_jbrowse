58b00bfbc3b0be7b25421f58a19de36079779286
sdash <sdash@ncgr.org>
2016-09-07 17:13:11 -0500

     q q
    No message
    t
    
    :exit

--- lis_gbrowse_page.tpl.php
+++ lis_gbrowse_page.tpl.php
@@ -1,6 +1,5 @@
 <!-- Title and guide text  -->
 
-
 <!-- <iframe id="frameviewer" frameborder="0" width="1200" height="2500" scrolling="yes"  src="http://www.ncbi.nlm.nih.gov/pubmed?linkname=pubmed_pubmed_citedin&from_uid=15608283"  name="frameviewer"> -->
 <!--  ------------iframe gbrowse-----------------  -->
 <iframe id="frameviewer" frameborder="0" width="1200" height="2500" scrolling="yes"  src="<?php  echo($url_source);  ?>"  name="frameviewer">
